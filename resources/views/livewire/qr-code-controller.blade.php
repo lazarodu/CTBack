fdd51b75e6857bea1fbfd22fac05fb3c74f8b50b
lazarodu <lazarodu@gmail.com>
2022-11-13 16:58:01 -0300

    QrCode svg img svg 600

--- resources/views/livewire/qr-code-controller.blade.php
+++ resources/views/livewire/qr-code-controller.blade.php
@@ -1,7 +1,6 @@
 <div class="py-12 px-6">
     <div class="flex justify-center align-center px-96">
-        {!! QrCode::size(300)->generate(Auth::user()->id, Auth::user()->id.'.svg') !!}
-        <img src="{{Auth::user()->id.'.svg'}}" alt="qrcode" class="w-full" />
+        {!! QrCode::size(600)->generate(Auth::user()->id) !!}
     </div>
     <h1 class="flex  justify-center align-center text-lg">{{Auth::user()->name}}</h1>
     <h1 class="flex  justify-center align-center text-lg">Apresente este QrCode para registrar sua presença!</h1>
