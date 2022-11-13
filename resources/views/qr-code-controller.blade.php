8989acaa3f449ca78379483143c4aaee01d8fa5e
lazarodu <lazarodu@gmail.com>
2022-11-13 17:24:44 -0300

    Css

--- resources/views/qr-code-controller.blade.php
+++ resources/views/qr-code-controller.blade.php
@@ -6,7 +6,7 @@
     </x-slot>
     <div class="py-12 px-6">
         <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="flex flex-col justify-center align-center ">
+            <div class="flex justify-center align-center ">
                 {!! QrCode::size(300)->generate(Auth::user()->id) !!}
             </div>
             <h1 class="flex  justify-center align-center text-lg">{{Auth::user()->name}}</h1>
