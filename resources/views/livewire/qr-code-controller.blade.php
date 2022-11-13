2a17d85e09876f902c6f4dd52f5262fb96a451a5
lazarodu <lazarodu@gmail.com>
2022-11-13 16:10:02 -0300

    QrCode png

--- resources/views/livewire/qr-code-controller.blade.php
+++ resources/views/livewire/qr-code-controller.blade.php
@@ -1,6 +1,6 @@
 <div class="py-12 px-6">
     <div class="flex justify-center align-center px-96">
-        {!! QrCode::size(300)->generate(Auth::user()->id) !!}
+        {!! QrCode::format('png')->size(300)->generate(Auth::user()->id) !!}
     </div>
     <h1 class="flex  justify-center align-center text-lg">Apresente este QrCode para registrar sua presença!</h1>
 </div>
