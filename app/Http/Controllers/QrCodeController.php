91221ce684ccc8ebf4a6b4397b63d1c79956d6f0
lazarodu <lazarodu@gmail.com>
2022-11-13 17:15:07 -0300

    QrCode svg img svg header

--- app/Http/Controllers/QrCodeController.php
+++ app/Http/Controllers/QrCodeController.php
@@ -8,6 +8,6 @@ class QrCodeController extends Controller
 {
     public function index()
     {
-        return view('qr-code-controller');
+        return view('qr-code-controller')->layout('layouts.app');
     }
 }
--- resources/views/qr-code-controller.blade.php
+++ resources/views/qr-code-controller.blade.php
@@ -1,7 +1,14 @@
-<div class="py-12 px-6">
-    <div class="flex justify-center align-center px-96">
-        {!! QrCode::size(300)->generate(Auth::user()->id) !!}
+<x-app-layout>
+    <x-slot name="header">
+        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
+            QrCode
+        </h2>
+    </x-slot>
+    <div class="py-12 px-6">
+        <div class="flex justify-center align-center px-96">
+            {!! QrCode::size(300)->generate(Auth::user()->id) !!}
+        </div>
+        <h1 class="flex  justify-center align-center text-lg">{{Auth::user()->name}}</h1>
+        <h1 class="flex  justify-center align-center text-lg">Apresente este QrCode para registrar sua presença!</h1>
     </div>
-    <h1 class="flex  justify-center align-center text-lg">{{Auth::user()->name}}</h1>
-    <h1 class="flex  justify-center align-center text-lg">Apresente este QrCode para registrar sua presença!</h1>
-</div>
+</x-app-layout>
